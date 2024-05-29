5a13b15e0f8f6fab36871c15ee9c2eaf31e3829a
Omar Ak <omarabedk@gmail.com>
2024-05-29 16:22:33 +0200

    repaire code

--- window.py
+++ window.py
@@ -7,6 +7,8 @@ class Window(QtWidgets.QMainWindow):
         x, y = dimension
         self.setFixedWidth(x)
         self.setFixedHeight(y)
+
+        self.Create_menu_bar()
         
         self.central_widget = QtWidgets.QWidget()
         self.setCentralWidget(self.central_widget)
@@ -14,10 +16,19 @@ class Window(QtWidgets.QMainWindow):
         self.layout = QtWidgets.QVBoxLayout(self.central_widget)
         self.modes_and_stacked_layout = QtWidgets.QHBoxLayout()
 
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+
         self.Create_Camera_Widget()
         self.Create_Modes_Widget()
         self.Create_Stacked_Widget()
-        self.apply_styles()
+
+        self.Up_Bttn.clicked.connect(self.up_button_clicked)
+        self.Down_Bttn.clicked.connect(self.down_button_clicked)
+        self.Left_Bttn.clicked.connect(self.left_button_clicked)
+        self.Right_Bttn.clicked.connect(self.right_button_clicked)
+
+        with open("styles.css", "r") as file:
+            self.setStyleSheet(file.read())
 
     def Create_Camera_Widget(self):
         self.camera_widget = QtWidgets.QWidget(objectName="camera_widget")
@@ -48,7 +59,7 @@ class Window(QtWidgets.QMainWindow):
         self.modes_widget = QtWidgets.QWidget(objectName="modes_widget")
         self.modes_layout = QtWidgets.QGridLayout(self.modes_widget)
         self.modes_label = QtWidgets.QLabel('      Modes')
-        self.modes_label.setFixedHeight(100)
+        self.modes_label.setFixedHeight(50)
         self.Manual_Button = QtWidgets.QPushButton('Manual')
         self.Tracking_Button = QtWidgets.QPushButton('Tracking')
         self.Random_Button = QtWidgets.QPushButton('Random')
@@ -65,53 +76,63 @@ class Window(QtWidgets.QMainWindow):
         self.stacked_widget.setFixedWidth(800)
         self.stacked_widget.setFixedHeight(400)
         
-        page1 = QtWidgets.QWidget(objectName="page1")
-        page2 = QtWidgets.QWidget(objectName="page2")
-        page3 = QtWidgets.QWidget(objectName="page3")
+        self.page1 = QtWidgets.QWidget(objectName="page1")
+        self.page2 = QtWidgets.QWidget(objectName="page2")
+        self.page3 = QtWidgets.QWidget(objectName="page3")
         
-        self.stacked_widget.addWidget(page1)
-        self.stacked_widget.addWidget(page2)
-        self.stacked_widget.addWidget(page3)
+        self.stacked_widget.addWidget(self.page1)
+        self.stacked_widget.addWidget(self.page2)
+        self.stacked_widget.addWidget(self.page3)
         
-        page1_Vlayout = QtWidgets.QVBoxLayout(page1)
+        page1_Vlayout = QtWidgets.QVBoxLayout(self.page1)
         page1_Hlayout1 = QtWidgets.QHBoxLayout()
         page1_Hlayout2 = QtWidgets.QHBoxLayout()
-        page2_layout = QtWidgets.QVBoxLayout(page2)
-        page3_layout = QtWidgets.QVBoxLayout(page3)
+        page2_layout = QtWidgets.QVBoxLayout(self.page2)
+        page3_layout = QtWidgets.QVBoxLayout(self.page3)
         
         spacer1 = QtWidgets.QWidget()
         spacer1.setFixedSize(50, 50)
         page1_Hlayout1.addWidget(spacer1)
-        Up_Bttn = QtWidgets.QPushButton("up")
-        Up_Bttn.setFixedWidth(100)
-        Up_Bttn.setFixedHeight(100)
-        page1_Hlayout1.addWidget(Up_Bttn)
+        self.Up_Bttn = QtWidgets.QPushButton()
+        self.Up_Bttn.setIcon(QtGui.QIcon('Icons/up.png'))
+        self.Up_Bttn.setIconSize(QtCore.QSize(80, 80))  # Adjust size as needed
+        self.Up_Bttn.setFixedWidth(100)
+        self.Up_Bttn.setFixedHeight(100)
+        page1_Hlayout1.addWidget(self.Up_Bttn)
         spacer2 = QtWidgets.QWidget()
         spacer2.setFixedSize(50, 50)
         page1_Hlayout1.addWidget(spacer2)
         page1_Vlayout.addLayout(page1_Hlayout1)
-        Left_Bttn = QtWidgets.QPushButton("left")
-        Left_Bttn.setFixedWidth(100)
-        Left_Bttn.setFixedHeight(100)
-        Down_Bttn = QtWidgets.QPushButton("down")
-        Down_Bttn.setFixedWidth(100)
-        Down_Bttn.setFixedHeight(100)
-        Right_Bttn = QtWidgets.QPushButton("right")
-        Right_Bttn.setFixedWidth(100)
-        Right_Bttn.setFixedHeight(100)
-        page1_Hlayout2.addWidget(Left_Bttn)
-        page1_Hlayout2.addWidget(Down_Bttn)
-        page1_Hlayout2.addWidget(Right_Bttn)
+        self.Left_Bttn = QtWidgets.QPushButton()
+        self.Left_Bttn.setIcon(QtGui.QIcon('Icons/left.png'))
+        self.Left_Bttn.setIconSize(QtCore.QSize(80, 80))  # Adjust size as needed
+        self.Left_Bttn.setFixedWidth(100)
+        self.Left_Bttn.setFixedHeight(100)
+        self.Down_Bttn = QtWidgets.QPushButton()
+        self.Down_Bttn.setIcon(QtGui.QIcon('Icons/down.png'))
+        self.Down_Bttn.setIconSize(QtCore.QSize(80, 80))  # Adjust size as needed
+        self.Down_Bttn.setFixedWidth(100)
+        self.Down_Bttn.setFixedHeight(100)
+        self.Right_Bttn = QtWidgets.QPushButton()
+        self.Right_Bttn.setIcon(QtGui.QIcon('Icons/right.png'))
+        self.Right_Bttn.setIconSize(QtCore.QSize(80, 80))  # Adjust size as needed
+        self.Right_Bttn.setFixedWidth(100)
+        self.Right_Bttn.setFixedHeight(100)
+        page1_Hlayout2.addWidget(self.Left_Bttn)
+        page1_Hlayout2.addWidget(self.Down_Bttn)
+        page1_Hlayout2.addWidget(self.Right_Bttn)
         page1_Vlayout.addLayout(page1_Hlayout2)
         page2_layout.addWidget(QtWidgets.QLabel("You are currently on Tracking mode ..."))
         page3_layout.addWidget(QtWidgets.QLabel("You are currently on Random mode ..."))
 
-        self.Manual_Button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.Manual_Button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0) and self.setFocusPolicy(QtCore.Qt.StrongFocus))
         self.Tracking_Button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
         self.Random_Button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(2))
 
+    
 
-        def keyPressEvent(self, event):
+    def keyPressEvent(self, event):
+        if self.stacked_widget.currentWidget() == self.page1:
             if event.key() == QtCore.Qt.Key_Up:
                 self.Up_Bttn.click()
             elif event.key() == QtCore.Qt.Key_Down:
@@ -120,70 +141,25 @@ class Window(QtWidgets.QMainWindow):
                 self.Left_Bttn.click()
             elif event.key() == QtCore.Qt.Key_Right:
                 self.Right_Bttn.click()
-            else:
-                super(Window, self).keyPressEvent(event)
+        else:
+            super(Window, self).keyPressEvent(event)
 
-    def apply_styles(self):
-        self.setStyleSheet("""
-            QMainWindow {
-                background-color: #2E2E2E;
-                color: #E0E0E0;
-                font-family: Arial, sans-serif;
-                font-size: 14px;
-            }
-            QLabel {
-                color: #E0E0E0;
-                font-size: 16px;
-            }
-            QPushButton {
-                background-color: #4CAF50;
-                border: none;
-                color: white;
-                padding: 10px 20px;
-                text-align: center;
-                text-decoration: none;
-                font-size: 16px;
-                margin: 4px 2px;
-                border-radius: 12px;
-            }
-            QPushButton:hover {
-                background-color: #45a049;
-            }
-            QPushButton:pressed {
-                background-color: #3e8e41;
-            }
-            QWidget#camera_widget {
-                padding: 10px;
-                border-radius: 10px;
-                background-color: #666;
-                margin: 10px;
-            }
-            QWidget#modes_widget {
-                padding: 10px;
-                border-radius: 10px;
-                background-color: #666;
-                margin: 10px;
-            }
-            QStackedWidget {
-                padding: 10px;
-                border-radius: 10px;
-                background-color: #666;
-                margin: 10px;
-            }
-            QWidget#page1 {
-                padding: 10px;
-                border-radius: 10px;
-                margin: 10px;
-            }
-            QWidget#page2 {
-                padding: 10px;
-                border-radius: 10px;
-                margin: 10px;
-            }
-            QWidget#page3 {
-                padding: 10px;
-                border-radius: 10px;
-                background-color: #666;
-                margin: 10px;
-            }
-        """)
+
+    def Create_menu_bar(self):
+        menu_bar = self.menuBar()
+        help_menu = menu_bar.addMenu('Help')
+        about_action = QtWidgets.QAction('About Us', self)
+        about_action.triggered.connect(self.show_about_dialog)
+        help_menu.addAction(about_action)
+
+    def show_about_dialog(self):
+        about_msg = QtWidgets.QMessageBox(self)
+        about_msg.setWindowTitle("About Us")
+        about_msg.setText("-------------------------------------------------\n"
+                          "This is a Robot Controller application created with PyQt5.\n"
+                          "-------------------------------------------------\n"
+                           "Omar ABDEL KADER | All Rights Reserved \u00A9")
+        about_msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        about_msg.setStyleSheet("QLabel {color: #000000;}")
+        about_msg.exec_()
+
